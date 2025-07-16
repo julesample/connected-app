0befd44ced985d0e92700e0ebb48c3901f1d7a99
Jules Orquin Lozada <111771206+julesample@users.noreply.github.com>
2025-07-16 22:17:53 +0800

    first commit

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -16,7 +16,9 @@ export default function AboutUs() {
         Users can create and share posts, follow others to see their content, and engage in private conversations. 
         With robust privacy controls, you can customize who sees your posts and manage your account's visibility. 
         Our goal is to provide a secure and personalized environment for you to interact with others and build meaningful connections.
+        Work in progress - we are constantly improving the platform to enhance user experience and add new features.
       </p>
+      <p>Owned and maintained by</p><a href="https://github.com/julesample">Julesample</a>
     </div>
   );
 }
